2ec4bd1166b62d259f5d62350b2591ccbde7e05b
Maureen ozakpumue <ozakpumuemaureen@gmail.com>
2025-02-22 12:05:20 +0100

    added webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { WaitList } from './pages/WaitList';
 import { WaitListSuccess } from './components/WaitListSuccess';
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -14,7 +13,6 @@ function App() {
           <Route path="/waitlist/success" element={<WaitListSuccess />} />
         </Routes>
       </div>
-    </Router>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+const basename = process.env.NODE_ENV === 'production' ? '/my-app' : '/';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
